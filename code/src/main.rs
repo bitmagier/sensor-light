d49d0d9044dbaa468d53c96e3335f0c90d4f151c
Roman Krüger <bitmagier@mailbox.org>
2024-02-10 23:23:47 +0100

    .

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,12 +1,86 @@
+use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
+use std::sync::RwLock;
 use anyhow::Result;
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::hal::delay::FreeRtos;
+use esp_idf_svc::hal::gpio::{InterruptType, Level, PinDriver, Pull};
 use esp_idf_svc::hal::prelude::Peripherals;
-use log::info;
-use rgb_led::{RGB8, WS2812RMT};
+
+const LED_MAX_POWER_LEVEL: usize = 100;
+enum Phase {
+    Off,
+    PowerDown,
+    PowerUp,
+    On
+}
+
+struct BarState {
+    dimm_phase: Phase,
+    led_power_level: usize,
+}
+
+impl Default for BarState {
+    fn default() -> Self {
+        BarState {
+            dimm_phase: Phase::Off,
+            led_power_level: 0,
+        }
+    }
+}
+
+
+
+fn interrupt_read() -> Result<()> {
+    let peripherals = Peripherals::take()?;
+    let mut pin = PinDriver::input(peripherals.pins.gpio12)?;
+    pin.set_pull(Pull::Floating)?;
+
+    log::info!("Radar Sensor - Interrupt Hello, world!");
+
+    static mut BAR_STATE: RwLock<BarState> = RwLock::new(BarState::default());
+
+    fn enter_dimm_out_phase() {
+        log::info!("DIMM out ..");
+    }
+
+    fn enter_dimm_on_phase() {
+        log::info!("DIMM on ..");
+    }
+
+    fn proceed_dimming() {
+        
+        let phase = LED_BAR_PHASE.load(Ordering::Relaxed);
+        match phase.as {
+            Phase::Off => assert_eq!(LED_POWER_LEVEL, 0),
+            Phase::PowerDown => {
+
+            }
+            Phase::PowerUp => {}
+            Phase::On => assert_eq!(LED_POWER_LEVEL, LED_MAX_POWER_LEVEL)
+        }
+    }
+
+    let callback = || {
+        let level = pin.get_level();
+        match level {
+            Level::Low => enter_dimm_out_phase(),
+            Level::High => enter_dimm_on_phase(),
+        }
+    };
+
+    pin.set_interrupt_type(InterruptType::AnyEdge)?;
+    unsafe { pin.subscribe(callback)? }
+    pin.enable_interrupt()?;
+
+    loop {
+        FreeRtos::delay_ms(500);
+        log::info!("power level (0..10): {}", LED_POWER_LEVEL.load(Ordering::Relaxed))
+    }
+}
+
 
 fn main() -> Result<()> {
-    log::info!("On.");
+    log::info!("on.");
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -17,23 +91,8 @@ fn main() -> Result<()> {
     let peripherals = Peripherals::take().unwrap();
     let _sysloop = EspSystemEventLoop::take()?;
 
-    log::info!("Configuring onboard LED");
-    
-    // Onboard RGB LED pin
-    // Rust ESP Board gpio2,  ESP32-C3-DevKitC-02 gpio8, ESP32-H2-DevKit-01 gpio8
-    let led = peripherals.pins.gpio8;
-    let channel = peripherals.rmt.channel0;
-    let mut ws2812 = WS2812RMT::new(led, channel)?;
-    
     loop {
-        info!("Red!");
-        ws2812.set_pixel(RGB8::new(255, 0, 0))?;
-        FreeRtos::delay_ms(1000);
-        info!("Green!");
-        ws2812.set_pixel(RGB8::new(0, 255, 0))?;
-        FreeRtos::delay_ms(1000);
-        info!("Blue!");
-        ws2812.set_pixel(RGB8::new(0, 0, 255))?;
+
         FreeRtos::delay_ms(1000);
     }
 }
